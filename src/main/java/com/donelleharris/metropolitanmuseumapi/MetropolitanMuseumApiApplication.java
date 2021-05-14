8a9693d3406743fe3e53ffbe755fb708d5b5e104
Donelle Harris <donelle.harris@gmail.com>
2021-05-13 21:21:15 -0500

    Initialize project & basic MVC structure

---
+++ src/main/java/com/donelleharris/metropolitanmuseumapi/MetropolitanMuseumApiApplication.java
@@ -0,0 +1,13 @@
+package com.donelleharris.metropolitanmuseumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetropolitanMuseumApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetropolitanMuseumApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/donelleharris/metropolitanmuseumapi/MetropolitanMuseumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donelleharris.metropolitanmuseumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetropolitanMuseumApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
